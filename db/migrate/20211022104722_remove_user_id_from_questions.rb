be9b9ada02c45fe7b743603ae4401f81fa195142
yusuke <tsunemi0217@icloud.com>
2021-12-11 14:04:09 +0900

    無駄な文章削除
    指摘してもらった無駄な文章を削除しました。

--- db/migrate/20211022104722_remove_user_id_from_questions.rb
+++ db/migrate/20211022104722_remove_user_id_from_questions.rb
@@ -3,7 +3,3 @@ class RemoveUserIdFromQuestions < ActiveRecord::Migration[6.1]
     remove_column :questions, :user_id, :string
   end
 end
-
-
-
-rails g model User nickname:string email:string password_digest:string admin:boolean
